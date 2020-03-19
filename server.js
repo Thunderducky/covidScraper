19ef120e77a877c77f43286e9cf122635b30858a
Eric Scott <Thunderducky@users.noreply.github.com>
2020-03-18 19:15:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const main = async () => {
     const holder2 = await holder.$$(".external-html");
     const data = await Promise.all(holder2.map(h => {
         return Promise.all([
-            h.$("h5 span").then(h => h.evaluate(element => parseInt(element.textContent).replace(/,/g, "")), h),
+            h.$("h5 span").then(h => h.evaluate(element => parseInt(element.textContent.replace(/,/g, ""))), h),
             h.$("h5 span:nth-child(3)").then(h => h.evaluate(element => element.textContent), h)
         ])
     }))
